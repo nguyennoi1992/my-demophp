a01be32003477a444181280078b9ad3281160ed4
abc40197 <abc40197@2e391b3e-d7d8-b53f-9416-0ddb69077093>
2014-10-02 04:00:49 +0000

    Update error modules with eclipse

--- workspace/minimart/application/Bootstrap.php
+++ workspace/minimart/application/Bootstrap.php
@@ -16,6 +16,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         Zend_Session::start();
         
         Zend_Layout::startMvc();
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin(new Zend_Controller_Plugin_ErrorHandler(array(
+                                'module'     => 'error',
+                                'controller' => 'error',
+                                'action'     => 'error'
+        )));
         
     }
 }
